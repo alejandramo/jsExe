00b62c018234efe0fb8996939ffd2f6f0b2b1d10
alejandramo <alejandra.momari@gmail.com>
2019-06-04 01:43:41 -0500

    avance parte1

--- practica1.js
+++ practica1.js
@@ -5,6 +5,9 @@ var month = currentDate.getMonth() + 1
 var year = currentDate.getFullYear()
 console.log(" " + day + "/ " + month + "/" + year)
 
+// Otra manera
+console.log(Date());
+
 
 // Obten el área de un triángulo. Pidele los 3 lados al usuario
 function area (b,h){ 
@@ -14,13 +17,14 @@ var calArea= (b * h) / 2;
 alert("el área de tu triángulo es :" + calArea);
 }
 
+// función pidiendo los 3 lados
+
 
 // Voltea una string dada por el usuario
 function volteaString(){
     var string = prompt("introduce el texto que deseas invertir");
     alert(string.reverse());
 }
-
 
 // Voltea una string dada por el usuario sin usar el método de reverse
 var cadena = prompt("Escribe la frase que quieres invertir");
@@ -60,7 +64,7 @@ var unidad = prompt("Escribe si quieres convertir a C o F");
 */
 // Definimos el objeto al que se le agregará la propiedad
 var usuario={
-  nombre:'blarz',
+  nombre:'Germán',
   apellido:'Hernandez'
 }
 //Creamos la función
@@ -77,67 +81,29 @@ usuario.nuevaPropiedad = true;
 
 
 // Realiza la suma de los valores que de el usuario hasta recibir un valor negativo
-function sumaSuma (){
-  var numn = parseInt(prompt("Introduce un número positivo para sumar"));
-  var num1 = parseInt(prompt("Introduce un número positivo para sumar"));  
-  do {
-    var resultado = promt("La suma de "+ numn + " + " + num1 + "   es = " + (numn+num1)+ "<br/>" + 
-                    "Introduce otro número para sumar" );
-    sumaSuma();
-  }
-  while (numn>= 0);
-}
+function sumaMas(){
+  var num1 = parseInt(prompt('Introduce un número'))
+  var num2 = 0
 
+  while(num1 >= 0){
+    num2 += num1;
+    var num1 = parseInt(prompt('Ingresa un número'))
+    }
+    alert('La suma es: ' + num2 + "  // Para seguir sumando debes ingresar sólo números positivos");
+}
 
 
 // Para sumar dos números hasta que uno de los dos es negativo
 function sumaPositiva(){
-var n1 = parseInt(prompt("Introduce un número"));
-var n2 = parseInt(prompt("Introduce el segundo número"));
-if ( n1 >= 0 & n2>= 0)
-  alert("La suma es " + (n1+n2));
- 
- else 
-  alert("Lo sentimos, tienes que agregar un número positivo, inténtalo nuevamente introduciendo sólo números positivos");
-  sumaPositiva();
-}
-
-
-// Para sumar n números hasta que ingrese un negativo
-var num1 = parseInt(prompt("introduce un número"));
-var numSum = 0;
-
-while (num1>=0){
-  numSum += num1;
-  var num1 = parseInt(prompt("Ingresa un número"))
-}
-alert('La suma es '+ ' '+ numSum);
-
-
-/*
-function sumaPositiva(){
   var n1 = parseInt(prompt("Introduce un número"));
   var n2 = parseInt(prompt("Introduce el segundo número"));
-  if ( n1 >= 0 & n2>= 0)
+  if ( n1 >= 0 & n2>= 0){
     alert("La suma es " + (n1+n2));
-   
-   else 
+  }else{ 
     alert("Lo sentimos, tienes que agregar un número positivo, inténtalo nuevamente introduciendo sólo números positivos");
     sumaPositiva();
   }
-function sumaOno(){
-  numPos = parseInt(prompt("Introduce un número")
-  while (numPos >=0){
-    var n2= prompt("Introduce otro número")
-  } 
 }
-*/
-// Agregar un ciclo para recibir n números
-// Siempre que el número recibido sea + / hacer suma de los números / mostrar el resultado y pedir un nuevo número
-// Cuando el usuario introduzca un -, detener la suma y pedir que se introduzca un +
-
-
-
 
 
 // Convierte a mayúscula la primer letra de cada palabra en una cadena dada por el usuario
@@ -164,47 +130,53 @@ function multiple(valor, multiple){
 
 // Revisa cuantas veces se repite un caracter dado en una cadena dada
 
-
+function buscaCaracter(){
+  var cadenaC = prompt("Introduce tu texto");
+  var caracter = prompt("¿Qué letra quieres contar");
+  var contar = 0;
+  
+  for (var i = 0; i < cadenaC.length; i++) {
+      if(cadenaC[i].toLowerCase() === caracter) contar ++; 
+  }
+   alert ("Tu frase tiene: " + contar + "  " + caracter);
+}
 
 
 // Revisa cuantas veces se repite un valor dado en un arreglo
-function valorDeArreglo (valor, arr){
-  var valor = parseInt(prompt("Introduce un valor a buscar").value);
-  var valores = 0;
-  var arr = [];
-
-  while(valores >=0){
-    arr.push(valores);
-    valores = prompt("Introduce un valor numérico: "); 
+function busqueda(){
+  var x = [1, 2, 3, 4, 5, 6, 7, 3, 4, 4, 5, 5, 6];
+  var busquele = parseInt(prompt("Num"));
+  var j = 0;
+  for (i = 0; i < x.length; i++) {
+      if (x[i] == busquele) j++;
   }
-  var conta = 0;
-  for (i = 0; i < arr.length; i++){
-    if (arr[i] == valor){
-      conta ++;
-    }
-  }
-  alert(conta);
-};
-
-
-
+  alert(" El " + busquele + " se repite "+ j + " veces ")     
+}
 
 
 // Divide un número de 3 dígitos en sus centenas, decenas y unidades
 // 103 -> 1 centena, 0 decenas, 3 unidades
-
-function numCentenas(cantidad){
-  var num = cantidad.toString().split(' ');
-  var resultado = num[0]+" centenas, "+ num[1]+ " decenas," + num[2] + " unidades";
+function numCentenas(){
+  var numeroCompleto = prompt("Introduce un número de 3 cifras");
+  var resultado = parseInt(numeroCompleto[0])+" centenas, "+ parseInt(numeroCompleto[1])+ " decenas, " + parseInt(numeroCompleto[2]) + " unidades";
   alert(resultado);
 };
-numCentenas(563);
 
 
 // Regresa todos los caracteres que no sean letras de una cadena
 function puraLetra(){
-  var 
-};
+  var dameLaCadena = prompt(" Introduce texto y números")
+  var separaArr = [];
+  var noletras = /[A-Za-z]/;
+ 
+  for (var i = 0; i < dameLaCadena.length; i++) {
+    if (!dameLaCadena[i].match(noletras)) {
+        separaArr.push( dameLaCadena[i] ); 
+    } 
+  }
+  alert ((separaArr.join(" "))+ " no son letras");
+}
+
 
 
 
@@ -212,26 +184,42 @@ function puraLetra(){
 function errOr() {
   var mensajeU = prompt("Escribe tu mensaje");
   console.error(mensajeU);
-
+}
 
 // Extiende la función anterior para atrapar el error e imprimir su mensaje y stack
 function errOr() {
   var mensajeU = prompt("Escribe tu mensaje");
-    try {
+  try{
     mensajeU();
   } catch(error){
-    console.error(mensajeU);
-};
+  console.error(mensajeU);
+}
+
 
 // Suma los contenidos de un arreglo de números
-function sumaNumArr{
-  var ponNum = parent(prompt("Introduce un número a sumar"))
-  var arr = [ ]
+function sumaNumArr(){
+  var ponNum = [2, 3, 8, 1, 20, 13];
+  var arr = 0
+  var i = 0
 
-
-
+  for (i=0 ; i<ponNum.length; i++) {
+    arr = arr + ponNum[i];
+  } alert ("La suma de cada número del arreglo es " + arr)
 }
+
+
 // Regresa un arreglo nuevo con el cuadrado de cada valor del arreglo original
+function arralCuadrado(){
+  var arregloexponencial = [2, 3, 4, 5, 6];
+  var arr = []
+  for (i=2; i<=arregloexponencial.length; i++) {
+    arr.push(Math.pow([i], 2));
+  } alert (arr)
+}
+// HAY ERROR / No se imprime el cuadrado el último número
+
+
+
 // Regresa que tipo de ángulo es el dado
 /*
   Agudo: 0 a 90 grados
@@ -239,13 +227,66 @@ function sumaNumArr{
   Obtuso: 90 a 180 grados
   Llano: 180 grados
 */
+function calculaAngulo(){
+  var angulo= parseInt(prompt("Ingresa los grados de tu ángulo"))
+  if(angulo < 90 & angulo > 0){
+    alert("Un ángulo de: "+ angulo + " º es agudo")
+  }else if(angulo === 90){
+    alert("Un ángulo de: "+ angulo + " º es recto")
+  }else if(angulo > 90 & angulo <180){
+    alert("Un ángulo de: "+ angulo + " º es obtuso")
+  }else if(angulo === 180){
+    alert("Un ángulo de: "+ angulo + " º es llano")
+  } else{
+    alert("Los grados ingresados deben estar entre 0 y 180")
+  }
+} 
+
 // Regresa un arreglo nuevo sin los valores repetidos de un arreglo original
+function devuelveArr(){
+  var arr1 = [1, 2, 4, 6, 3, 2, 4, 2, 7, 8, 9]
+  var arrNuevo = [...new Set(arr1)];
+  alert("El nuevo arreglo es: " + arrNuevo);
+}
+
+
 // Quita los valores repetidos de un arreglo (sin usar otro arreglo)
+
+/*
+function quitaElementos(){
+  var arrcero = [1, 4, 5, 2, 3, 5, 6, 5, 6, 7, 7, 9]
+  var i,
+  for(i=0; i<arreglo.length; i++){
+    obj[arrcero[i]]=0;
+  }
+  alert(arrcero)
+}
+
+ var arrcero = [1, 4, 5, 2, 3, 5, 6, 5, 6, 7, 7, 9];
+Array.prototype.unique= function(arrcero){
+  return function(){return this.filter(arrcero)}}(function(a, b, c){return c.indexOf(a,b+1)<0});
+  alert(arrcero)
+*/
+
 // Convierte un número binario dado por el usuario a decimal
+function convierteaDe(){
+  var numBin = parseInt(prompt("Introduce un número binario"))
+  alert(numBin.toString(10))
+}
 
 // Convierte un número decimal dado por el usuario a binario, octal y hexadecimal (bases 2, 8 y 16)
+function convierteavarios(){
+  var numDecimal = parseInt(prompt("Introduce un número binario"))
+  alert("El numero decimal " + numDecimal + " es " + numDecimal.toString(2) + " en binario.");
+  alert("El numero decimal " + numDecimal + " es " + numDecimal.toString(8) + " en octal.");
+  alert("El numero decimal " + numDecimal + " es " + numDecimal.toString(16) + " en hexadecimal."); 
+}
+
 // Regresa la cantidad de valores que comparten dos arreglos diferentes
+
+
 // Valida que una cadena dada no tenga espacios en blanco
+
 
 // Dada una cadena, determina su valor de scrabble
 /*
